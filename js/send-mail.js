382f16c49efa0f28c202ae63d3bfc71704b02bb0
otngotalone99 <56649477+otngotalone99@users.noreply.github.com>
2020-12-23 22:53:52 +0700

    send mail with formspree

--- js/send-mail.js
+++ js/send-mail.js
@@ -1,18 +1,57 @@
-$(document).ready(function () {
-    $("#contact-form").on('submit', function (event) {
-        event.preventDefault();
-        var formData = $(this).serialize();
-        $.ajax({
-            type: 'POST',
-            url: 'sendmail.php',
-            dataType: "json",
-            data: formData,
-            success: function (response) {
-                alert(response.success);
-            },
-            error: function (xhr, status, error) {
-                console.log(xhr);
-            }
-        });
-    });
-});
+(function ($) {
+	
+	$(function () {
+		var contactForm = $('#contact-form');
+		var loader = contactForm.find('.ajax-loader');
+
+		$(".submit").click(function (e)
+		{
+			if (contactForm.valid())
+			{
+				loader.css('display', 'block');
+
+				// handle the form submission event
+				e.preventDefault();
+
+				$.ajax({
+					url: "https://formspree.io/f/xnqoodvl",
+					type: "POST",
+					dataType: "json",
+					data: {
+						name: $("#name").val(),
+						email: $("#email").val(),
+						subject: $("#subject").val(),
+						message: $("#message").val()
+						
+					},
+					success: function (data) {
+						loader.hide();
+						contactForm.clearForm();
+					},
+					error: function (error) {
+						$('.alert').addClass('error');
+					}
+				});
+
+				$('.alert').fadeIn().delay(2000).fadeOut();
+			}
+			
+			return false
+		});
+	
+	});
+	
+	$.fn.clearForm = function() {
+	  return this.each(function() {
+	    var type = this.type, tag = this.tagName.toLowerCase();
+	    if (tag == 'form')
+	      return $(':input',this).clearForm();
+	    if (type == 'text' || type == 'password' || tag == 'textarea')
+	      this.value = '';
+	    else if (type == 'checkbox' || type == 'radio')
+	      this.checked = false;
+	    else if (tag == 'select')
+	      this.selectedIndex = -1;
+	  });
+	};
+})(jQuery);
